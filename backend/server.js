19ba58a3c834cd91cfc2d5f5f6e2c3744f7a56a1
Felix Owolabi <owolabifelix78@gmail.com>
2023-05-27 11:59:07 +0100

    MERN Stack

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,16 @@ mongoose.connect(process.env.MONGOURI)
 app.use('/workout',workoutRoutes);
 app.use(userAuth);
 
+// VERCEL Deployment
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+
